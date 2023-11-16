a687a65aa13d43fc5ec8e6da321b432f606ebf69
Thom2503 <thomveldhuis03@gmail.com>
2023-11-16 19:37:36 +0100

    #14 footer met uitlog knop toegevoegd dus je kan nu gemakkelijk uitloggen

---
+++ client/src/components/Footer.js
@@ -0,0 +1,23 @@
+import React, { Component } from 'react';
+import { deleteCookie } from '../include/util_functions';
+
+export class Footer extends Component {
+	
+	loguitEvent() {
+		// verwijder de cookies die de gebruiker krijgt aan het begin
+		deleteCookie("user");
+		deleteCookie("isadmin");
+
+		window.location.replace("login");
+	}
+
+    render() {
+        return (
+			<footer>
+				<a onClick={() => this.loguitEvent()} className='loguit-button'>
+					<span className='loguit-text'>Loguit</span>
+				</a>
+			</footer>
+        );
+    }
+}
