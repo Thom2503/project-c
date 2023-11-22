1df23cbcf3bedb99db7d711fc893bc4b47a37200
jimmy <jimmytrxn@protonmail.com>
2023-11-22 20:54:47 +0100

    + Fetching news works + Solved issues relating for not displaying news.

--- client/src/components/NieuwsDetails.js
+++ client/src/components/NieuwsDetails.js
@@ -4,7 +4,7 @@ export class NieuwsDetails extends Component {
     static displayName = NieuwsDetails.name;
     constructor(props) {
         super(props);
-        this.state = {data: []};
+        this.state = { data: [], filteredData: null };
     }
 
     componentDidMount() {
@@ -12,28 +12,37 @@ export class NieuwsDetails extends Component {
     }
 
     async fetchNewsDetails() {
-            const response = await fetch('../news');
-            const data = await response.json();
-            this.setState({ data: data });
+        const response = await fetch('../news');
+        const data = await response.json();
+        this.setState({ data: data });
+
+        // Get the URL parameter
+        const urlParams = new URLSearchParams(window.location.search);
+        const urlId = parseInt(urlParams.get("ID"), 10);
+
+        // Filter data based on the URL parameter
+        const filteredData = data.filter(news => news.NewsID === urlId);
+
+        if (filteredData.length > 0) {
+            this.setState({ filteredData: filteredData[0] });
+        }
     }
 
     render() {
         return (
             <>
-                {this.state.data.length > 0 ? (
-                    this.state.data.map((news, index) => (
-                        <div className="w-[100%] sm:w-[95%] m-auto">
-                            <h1 className="text-[#848484] mb-4">{news.PostTime}</h1>
-                            <div className="m-2 bg-gray-100 p-6">
-                                <p className="text-[#848484] mt-2 text-right">Gemaakt door:</p>
-                                <span className="text-[#848484] mt-2 text-right">{news.PostTime}</span>
-                                <div style={{ position: 'relative', left: '10px', bottom: '-150px' }}>
-                                    <h1 className="text-[#848484] font-bold text-[18px] mt-2 text-left inline-block align-text-bottom">{news.Title}</h1>
-                                    <p className="text-[#848484]">{news.Description}</p>
-                                </div>
+                {this.state.filteredData ? (
+                    <div className="w-[100%] sm:w-[95%] m-auto">
+                        <h1 className="text-[#848484] mb-4">{this.state.filteredData.Title}</h1>
+                        <div className="m-2 bg-gray-100 p-6">
+                            <p className="text-[#848484] mt-2 text-right">Gemaakt door:</p>
+                            <span className="text-[#848484] mt-2 text-right">{this.state.filteredData.PostTime}</span>
+                            <div style={{ position: 'relative', left: '10px', bottom: '-150px' }}>
+                                <h1 className="text-[#848484] font-bold text-[18px] mt-2 text-left inline-block align-text-bottom">{this.state.filteredData.Title}</h1>
+                                <p className="text-[#848484]">{this.state.filteredData.Description}</p>
                             </div>
                         </div>
-                    ))
+                    </div>
                 ) : (
                     <p>Geen nieuws gevonden.</p>
                 )}
