9c32b5f179c791957a8430da6caebcb0fbf3d9fa
Thom2503 <thomveldhuis03@gmail.com>
2023-11-30 19:18:04 +0100

    Kamers dynamisch component laten laden wanneer je op een kamer klikt

--- client/src/components/Sidebar.js
+++ client/src/components/Sidebar.js
@@ -6,19 +6,31 @@ export class Sidebar extends Component {
 
 	constructor(props) {
 		super(props);
-		this.state = { type: props.type };
+		this.state = {
+			isOpen: props.isOpen,
+			type: Number.parseInt(props.type),
+			room: props.room ?? null,
+			openID: props.id
+		};
 	}
 
+	componentDidUpdate(prevProps) {
+		this.state.isOpen = !prevProps.room.isOpen;
+	}
 
 	async getUsersInRoom() {
-
+		const response = await fetch(`rooms/${this.state.room.RoomsID}`);
+		const data = await response.json();
 	}
 
 	render() {
+		if ((this.state.room.isOpen ?? false) === false) return;
 		switch (this.state.type) {
 		case 1:
 			return (
 				<div className="kamers sidebar">
+					<h3>{this.state.room.Name}</h3>
+					<hr />
 				</div>
 			);
 		default:
