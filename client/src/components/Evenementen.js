c1921678387f4fc026c1a066a79f4e5b24f78075
Jimmy-Tran <jimmytrxn@protonmail.com>
2023-12-03 15:54:43 +0100

    in progress event modal.

--- client/src/components/Evenementen.js
+++ client/src/components/Evenementen.js
@@ -1,24 +1,43 @@
 import React, { Component } from 'react';
 import { Header } from './Header';
 import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
-import {faCalendar, faClock, faSquarePlus, faUser} from "@fortawesome/free-solid-svg-icons";
-import {DatePicker, LocalizationProvider} from '@mui/x-date-pickers';
+import {
+    faCalendar,
+    faCircle,
+    faCirclePlus,
+    faClock,
+    faSquarePlus,
+    faUser,
+    faXmark
+} from "@fortawesome/free-solid-svg-icons";
+import {DatePicker, DesktopTimePicker, LocalizationProvider, TimePicker} from '@mui/x-date-pickers';
 import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs'
 import { getCookie } from '../include/util_functions';
-import {Link} from "react-router-dom";
+import Drawer from '@mui/material/Drawer';
+import {Checkbox, FormControl, FormControlLabel, InputLabel, MenuItem, Select, TextField} from "@mui/material";
 
 export class Evenementen extends Component {
     static displayName = Evenementen.name;
 
-    constructor(props){
+    constructor(props) {
         super(props);
-        this.state = {data: []};
-        document.title = "Events"
+        this.state = {
+            data: [],
+            rooms: [],
+            isSidebarOpen: false  // Added state for sidebar
+        };
+        document.title = "Events";
     }
 
     componentDidMount() {
         this.getEvents();
+        this.getRooms();
     }
+
+    toggleSidebar = () => {
+        this.setState((prevState) => ({ isSidebarOpen: !prevState.isSidebarOpen }));
+    }
+
 
     async getEvents() {
         const response = await fetch('events');
@@ -26,7 +45,16 @@ export class Evenementen extends Component {
         this.setState({ data: data });
     }
 
+    async getRooms() {
+        const response = await fetch('/rooms');
+        const roomsData = await response.json();
+        this.setState({ rooms: roomsData });
+    }
+
+
+
     render() {
+
 
         return (
             <>
@@ -39,21 +67,38 @@ export class Evenementen extends Component {
 
 
                             {getCookie("isadmin") !== "true" && (
-                                <Link  to="?modal=3" className='bg-purple-500 h-full rounded-[5px] text-white font-medium p-4'>
-                                    <FontAwesomeIcon icon={faSquarePlus} /> Evenement Toevoegen
-                                </Link >
+                                <a href="?modal=3" className='h-full text-[23px] gap-2 text-[#8A8A8A] font-normal cursor-pointer flex justify-center items-center'
+                                     onClick={this.toggleSidebar}
+                                >
+                                    Evenement Toevoegen <FontAwesomeIcon icon={faCirclePlus} />
+                                </a>
                             )}
                             <LocalizationProvider dateAdapter={AdapterDayjs}>
                                 <DatePicker />
                             </LocalizationProvider>
                         </div>
                     </div>
+                    {/*<Drawer*/}
+                    {/*    anchor="right"*/}
+                    {/*    open={this.state.isSidebarOpen}*/}
+                    {/*    onClose={this.toggleSidebar}*/}
+                    {/*    PaperProps={{ style:*/}
+                    {/*            {*/}
+                    {/*                borderTopLeftRadius: '10px',*/}
+                    {/*                borderBottomLeftRadius: '10px',*/}
+                    {/*                background: '#F9F9F9',*/}
+                    {/*                boxShadow: '0px 4px 4px 0px rgba(0, 0, 0, 0.25)'*/}
+
+                    {/*            } }}*/}
+                    {/*    hideBackdrop='true'*/}
+                    {/*>*/}
+                    {/*</Drawer>*/}
                     <div className="gap-5 grid grid-cols-2">
                         {this.state.data.length > 0 ? (
                                 this.state.data.map((event, index) => (
                                     <div className="bg-[#F9F9F9] sm:mx-[20px] max-w-[1200px] w-[100%] h-[150px] p-4 flex flex-col justify-center rounded-xl border-[2px]">
                                         <div className="">
-                                            <h1 className="text-[#792F82] font-medium text-[23px]">{event.Title} <span className=" px-[9px] py-[3px] bg-[#BAFFA1] rounded-[100px] p-1 text-[#02BB15] text-[13px]"><small className="rounded-[100px] text-[20px]">●</small>Internal</span></h1>
+                                            <h1 className="text-[#792F82] font-medium text-[23px]">{event.Title} <span className=" px-[9px] py-[3px] bg-[#BAFFA1] rounded-[100px] p-1 text-[#02BB15] text-[13px]">Internal</span></h1>
                                             <span className="text-[#848484] text-[14px]">Klik voor meer informatie</span>
                                         </div>
                                         <div className="mt-auto flex flex-row gap-8">
@@ -71,8 +116,8 @@ export class Evenementen extends Component {
                                                     <FontAwesomeIcon icon={faCalendar} className="text-[#D8D8D8]"/>
                                                 </div>
                                                 <div className="flex flex-col">
-                                                    <span className="text-[#B0B0B0] text-[12px]">Host</span>
-                                                    <span className="text-[#5F5F5F] text-[13px] font-bold ">Name</span>
+                                                    <span className="text-[#B0B0B0] text-[12px]">Time</span>
+                                                    <span className="text-[#5F5F5F] text-[13px] font-bold ">{event.Time}</span>
                                                 </div>
                                             </div>
                                             <div className="flex flex-row items-center gap-2">
@@ -80,8 +125,8 @@ export class Evenementen extends Component {
                                                     <FontAwesomeIcon icon={faClock} className="text-[#D8D8D8]"/>
                                                 </div>
                                                 <div className="flex flex-col">
-                                                    <span className="text-[#B0B0B0] text-[12px]">Host</span>
-                                                    <span className="text-[#5F5F5F] text-[13px] font-bold">Name</span>
+                                                    <span className="text-[#B0B0B0] text-[12px]">Date</span>
+                                                    <span className="text-[#5F5F5F] text-[13px] font-bold">{event.Date}</span>
                                                 </div>
                                             </div>
 
--- client/src/components/EventModal.js
+++ client/src/components/EventModal.js
@@ -1,15 +1,38 @@
 import React, { Component } from "react";
 import "../css/voorzieningen.css";
+import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
+import {faXmark} from "@fortawesome/free-solid-svg-icons";
+import {
+  Box,
+  Checkbox,
+  FormControl,
+  FormControlLabel,
+  InputLabel,
+  MenuItem,
+  Select,
+  Tab,
+  Tabs,
+  TextField
+} from "@mui/material";
+import {DatePicker, LocalizationProvider, TimePicker} from "@mui/x-date-pickers";
+import {AdapterDayjs} from "@mui/x-date-pickers/AdapterDayjs";
 
 export class EventModal extends Component {
   static displayName = EventModal.name;
 
   constructor(props) {
     super(props);
-    this.state = { name: "", total: 0, deleteSupply: false, supply: 0 };
+    this.state = { rooms: [], name: "", total: 0, deleteSupply: false, supply: 0 };
+
 
     this.handleChange = this.handleChange.bind(this);
     this.handleSubmit = this.handleSubmit.bind(this);
+    this.getRooms();
+  }
+  async getRooms() {
+    const response = await fetch('/rooms');
+    const roomsData = await response.json();
+    this.setState({ rooms: roomsData });
   }
 
   componentDidMount() {
@@ -27,6 +50,12 @@ export class EventModal extends Component {
       this.setState({ [event.target.name]: event.target.value });
     }
   }
+
+  handleTabs = (event, newValue) => {
+    setValue(newValue);
+  }
+
+
 
   async handleSubmit(event) {
     event.preventDefault();
@@ -66,6 +95,7 @@ export class EventModal extends Component {
   }
 
   async fetchSupplyData(supplyID) {
+    const [value, setValue] = React.useState('one');
     try {
       const response = await fetch(`supplies/${supplyID}`);
       const data = await response.json();
@@ -85,46 +115,83 @@ export class EventModal extends Component {
   render() {
     return (
       <div className="modal">
-        <h2>Evenement Toevoegen</h2>
-        <div className="modal-content">
-          <form onSubmit={this.handleSubmit}>
-            <div className="input-field-div">
-              <label htmlFor="supplyName" className="input-field-label">Naam:</label>
-              <input
-                type="text"
-                id="supplyName"
-                name="name"
-                className="input-field"
-                value={this.state.name}
-                onChange={this.handleChange}
-                required
+          <div className={'mt-4 p-7'}>
+            <h3 className='font-medium text-[#792F82] text-[25px] border-b-[1px] border-b-[#E8E8E8]'>Toevoegen</h3>
+            <div className='mt-5 flex flex-col gap-4'>
+              <div className='grid grid-cols-2 gap-2'>
+                <TextField id="outlined-basic"
+                           placeholder="Typ hier je titel."
+                           label="Titel" variant="outlined" className={"w-[100%]"}/>
+                <LocalizationProvider dateAdapter={AdapterDayjs}>
+                  <DatePicker />
+                </LocalizationProvider>
+              </div>
+              <TextField
+                  id="outlined-textarea"
+                  label="Beschrijving"
+                  placeholder="Typ hier je beschrijving."
+                  multiline
+                  className='w-[100%]'
               />
-            </div>
-            <div className="input-field-div">
-              <label htmlFor="supplyTotal" className="input-field-label">Totaal:</label>
-              <input
-                type="number"
-                id="supplyTotal"
-                name="total"
-                className="input-field"
-                value={this.state.total}
-                onChange={this.handleChange}
-                required
+              <div className='grid grid-cols-2 gap-2'>
+                <LocalizationProvider dateAdapter={AdapterDayjs}>
+                  <TimePicker ampm={false} label="Start Tijd" />
+                </LocalizationProvider>
+                <LocalizationProvider dateAdapter={AdapterDayjs}>
+                  <TimePicker ampm={false} label="Eind Tijd" />
+                </LocalizationProvider>
+              </div>
+              <Tabs
+                  value={value}
+                  onChange={handleTabs}
+                  textColor="secondary"
+                  indicatorColor="secondary"
+                  aria-label="secondary tabs example"
+              >
+                <Tab value="one" label="Item One" />
+                <Tab value="two" label="Item Two" />
+                <Tab value="three" label="Item Three" />
+              </Tabs>
+              <FormControlLabel
+                  control={<Checkbox checked={this.state.showRooms} onChange={(e) => this.setState({ showRooms: e.target.checked })} />}
+                  label="Locatie De Loods"
               />
+              {this.state.showRooms ? (
+                  <FormControl fullWidth>
+                    <InputLabel id="demo-simple-select-label">Kamer</InputLabel>
+                    <Select
+                        labelId="demo-simple-select-label"
+                        id="demo-simple-select"
+                        label="Kamer"
+                        value={this.state.selectedRoom}
+                        onChange={(event) => this.setState({ selectedRoom: event.target.value })}
+                    >
+                      {this.state.rooms.map((room) => (
+                          <MenuItem key={room.RoomsID} value={room.Name}>
+                            {room.Name}
+                          </MenuItem>
+                      ))}
+                    </Select>
+                  </FormControl>
+              ) : (
+                  <TextField
+                      id="outlined-basic"
+                      placeholder="Adres"
+                      label="Adres"
+                      variant="outlined"
+                      className={"w-[100%]"}
+                  />
+              )}
+
+
+
+              <TextField id="outlined-basic"
+                         placeholder="Status"
+                         label="Status" variant="outlined" className={"w-[100%]"}/>
+
             </div>
-            <div className="input-field-div">
-              <label htmlFor="supplyDelete" >Delete:</label>
-              <input
-                type="checkbox"
-                id="supplyDelete"
-                name="deleteSupply"
-                value={this.state.deleteSupply}
-                onChange={this.handleChange}
-              />
-            </div>
-            <input type="submit" value="Opslaan" className="save-button"/>
-          </form>
-        </div>
+          </div>
+
       </div>
     );
   }
