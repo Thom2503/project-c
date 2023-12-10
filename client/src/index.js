e5dbb15e789b81cb8cd8c082855e8df990bb0dd8
Thom2503 <thomveldhuis03@gmail.com>
2023-12-10 19:06:22 +0100

    notificatie web push service workers weg doen werkt helaas niet en dus wordt een alternatief gevonden

--- client/src/index.js
+++ client/src/index.js
@@ -2,26 +2,13 @@ import React from 'react';
 import { createRoot } from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import App from './App';
-import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 import reportWebVitals from './reportWebVitals';
 
 const baseUrl = "/";
 const rootElement = document.getElementById('root');
 const root = createRoot(rootElement);
 
-// serviceWorkerRegistration.register();
-
-
 reportWebVitals();
-
-navigator.serviceWorker.register('service-worker.js');
-navigator.serviceWorker.ready.then((registration) => {
-	return registration.pushManager.getSubscription().then(async (subscription) => {
-		if (subscription) return subscription;
-
-		// krijg de subscription
-	});
-});
 
 root.render(
   <BrowserRouter basename={baseUrl}>
